6d67ebdae2b614f0a9ba386397fb2b0e1507bf83
tristan.wittgen <tristanwittgen@gmail.com>
2025-03-23 00:38:50 +0100

    try defining dirname

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve, dirname, path } from 'path'
+import path from 'path'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
