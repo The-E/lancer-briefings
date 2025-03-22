19aefb0452e5eedeefe173ef3536c5e31ee23df7
tristan.wittgen <tristanwittgen@gmail.com>
2025-03-23 00:37:03 +0100

    try defining dirname

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { resolve, dirname, path } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: "/lancer-briefings/",
   resolve: {
-
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   }
 })
