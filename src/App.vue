b0460a66d58bd7ae627dc2115af08ae180611eed
tristan.wittgen <tristanwittgen@gmail.com>
2025-03-29 23:35:20 +0100

    mission 1 success

--- src/App.vue
+++ src/App.vue
@@ -96,7 +96,7 @@ export default {
         {
           "slug": "001",
           "name": "Meteor Swarm",
-          "status": "start"
+          "status": "success"
         },
         {
           "slug": "002",
