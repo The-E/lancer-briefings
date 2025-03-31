cc7431a10e5de86fed485402bfd38e9fd99992d6
Fabian Woltermann <fwoltermann@gmail.com>
2025-03-31 21:25:55 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "./",
+  base: "/lancer-briefings/",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(import.meta.dirname, './src'),
     },
   }
 })
